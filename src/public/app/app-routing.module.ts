f7c890d0197f8a12c0cad4252789077dcd9992cf
Arpit Shah <ashah@iconectiv.com>
2017-07-28 23:36:22 -0400

    Create login page with Angular Material

--- src/public/app/app-routing.module.ts
+++ src/public/app/app-routing.module.ts
@@ -2,12 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { TaskListComponent } from './task-list/task-list.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: '/login'
+  },
+  {
+    path: 'login',
     children: [],
-    component: TaskListComponent
+    component: LoginComponent
   }
 ];
 
